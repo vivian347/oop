790a8556b204a3498e8d9059c49e804d098dab96
viv347 <vkinyua001@gmail.com>
2022-02-24 07:51:55 +0300

    Assignment1

---
+++ com/scm2110347/P1.java
@@ -0,0 +1,33 @@
+package com.scm2110347;
+
+import java.util.Scanner;
+
+
+
+
+public class P1 {
+    public static void main(String[] args){
+//        declare variables
+        float x,y,z,m;
+        float sum = 0;
+        float avg =0;
+//Prompt user to enter 3 numbers
+        Scanner n = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        x = n.nextFloat();
+        System.out.println("Enter second number: ");
+        y = n.nextFloat();
+        System.out.println("Enter last number: ");
+        z = n.nextFloat();
+//Calculate sum
+         m = x+y;
+         sum = Float.sum(m,z);
+//Calculate average
+        avg =sum/3;
+//Output
+        System.out.println("The sum is: " + sum);
+        System.out.println("The average is: "+ avg);
+    }
+
+
+}
---
+++ com/scm2110347/P2b.java
@@ -0,0 +1,30 @@
+package com.scm2110347;
+
+import java.util.Scanner;
+
+public class P2b {
+    public static void main(String[] args){
+
+        int month;
+        String monthName;
+        Scanner m = new Scanner(System.in);
+        System.out.println("Enter month number: ");
+        month = m.nextInt();
+        monthName = switch (month) {
+            case 1 -> "January";
+            case 2 -> "February";
+            case 3 -> "March";
+            case 4 -> "April";
+            case 5 -> "May";
+            case 6 -> "June";
+            case 7 -> "July";
+            case 8 -> "August";
+            case 9 -> "September";
+            case 10 -> "October";
+            case 11 -> "November";
+            case 12 -> "December";
+            default -> "Invalid month";
+        };
+        System.out.println("The month is: " +monthName);
+    }
+}
